f4472b3cff342781fa9aab41f09795f8a279651a
Rahul Bojanapally <rahul.b246@gmail.com>
2019-04-20 12:02:52 +0530

    ...

--- server.js
+++ server.js
@@ -1,9 +1,8 @@
 const express = require('express')
 const app = express()
-const port = process.env.PORT || 3000
+const port = process.env.PORT || 80
 
-app.use(express.static('public'))
 
-app.get('/', (req, res) => res.sendFile('index.html'))
+app.get('/', (req, res) => res.sendFile(__dirname+'/index.html'))
 
 app.listen(port, () => console.log(`Example app listening on port ${port}!`))
